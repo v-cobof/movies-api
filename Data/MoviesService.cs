6c25a135465bd235dc1317b5e90e2135cf4fa586
Victor Cobo <v.cobof@gmail.com>
2022-02-19 14:15:15 -0300

    finished CRUD

---
+++ Data/MoviesService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using moviesAPI.Models;
+
+namespace moviesAPI.Data
+{
+    public class MoviesService
+    {
+        private readonly IMongoCollection<Movie> _movies;
+
+        public MoviesService(IOptions<MoviesDatabaseSettings> options)
+        {
+            var mongoClient = new MongoClient(options.Value.ConnectionString);
+
+            _movies = mongoClient.GetDatabase(options.Value.DatabaseName).GetCollection<Movie>(options.Value.MoviesCollectionName);
+        }
+
+        public async Task<List<Movie>> GetAll() => await _movies.Find(_ => true).ToListAsync();
+
+        public async Task<Movie> Get(string id) => await _movies.Find(m => m.Id == id).FirstOrDefaultAsync();
+
+        public async Task Create(Movie newMovie) => await _movies.InsertOneAsync(newMovie);
+
+        public async Task Update(string id, Movie updateMovie) => await _movies.ReplaceOneAsync(m => m.Id == id, updateMovie);
+
+        public async Task Delete(string id) => await _movies.DeleteOneAsync(m => m.Id == id);
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+using moviesAPI.Data;
+using moviesAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MoviesDatabaseSettings>(builder.Configuration.GetSection("MoviesDatabaseSettings"));
+builder.Services.AddSingleton<MoviesService>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Movies API");
+
+app.MapGet("/api/movies", async (MoviesService moviesService) => await moviesService.GetAll());
+
+app.MapGet("/api/movies/{id}", async (MoviesService moviesService, string id) =>
+{
+    var movie = await moviesService.Get(id);
+
+    return movie is null ? Results.NotFound() : Results.Ok(movie);
+});
+
+app.MapPost("/api/movies", async (MoviesService moviesService, Movie movie) =>
+{
+    await moviesService.Create(movie);
+    return Results.Ok();
+});
+
+app.MapPut("/api/movies/{id}", async (MoviesService moviesService, string id, Movie updatedMovie) =>
+{
+    var movie = await moviesService.Get(id);
+    if(movie is null) return Results.NotFound();
+
+    updatedMovie.Id = movie.Id;
+    await moviesService.Update(id, updatedMovie);
+
+    return Results.Ok();
+});
+
+app.MapDelete("/api/movies/{id}", async (MoviesService moviesService, string id) =>
+{
+    Movie movie = await moviesService.Get(id);
+
+    if (movie is null) return Results.NotFound();
+
+    await moviesService.Delete(movie.Id);
+
+    return Results.NoContent();
+});
+
+app.Run();
